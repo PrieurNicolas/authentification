ea4d85550ee936a5f4e2a18598b713a25c78eee4
ZorfyEvo <nicolasprieur22@gmail.com>
2022-09-28 11:58:03 +0200

    oui

--- src/models/dbConfig.ts
+++ src/models/dbConfig.ts
@@ -3,8 +3,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "NeoR",
-    password: "b",
+    database: "authentification",
+    password: "440802",
     port: 5432,
 });
 
