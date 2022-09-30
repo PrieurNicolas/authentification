3b67d8558ef51967bfe66b17aa04a0ad75a0e4d0
Uverlink <marc.pizzanelli@gmail.com>
2022-09-30 11:24:29 +0200

    Corrected GET Users route

--- src/controller.ts
+++ src/controller.ts
@@ -34,15 +34,15 @@ const getUtilisateurs = (req: Request, res: Response) => {
   );
 };
 
-export function authenticateToken(req: Request,res: Response){ //},next: NextFunction){
+export function authenticateToken(req: Request,res: Response,next: NextFunction){
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if(token==null) return res.status(401).json({error: "Null token" });
   jwt.verify(token,process.env.ACCESS_TOKEN_SECRET as string, (error,user)=>{
     if (error) return res.status(403).json({error:error.message});
-    return res.status(201).json({user});
+    //return res.status(201).json({user});
     // req.user = user;
-    // next();
+    next();
   })
 }
 
