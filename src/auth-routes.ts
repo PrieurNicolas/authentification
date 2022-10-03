a99349d1023ffbbdbf708c2b60c8fd515648731a
Uverlink <marc.pizzanelli@gmail.com>
2022-10-03 12:21:48 +0200

    C3

--- src/auth-routes.ts
+++ src/auth-routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.get('/refresh_token', controller.refreshToken);
 router.delete('/refresh_token', controller.deleteToken);
+router.post('/login', controller.loginUtilisateur);
 
 module.exports = router;
