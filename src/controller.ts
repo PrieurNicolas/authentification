4c3a071922b6d1cac56299c2b6c38ef9efc2836f
Uverlink <marc.pizzanelli@gmail.com>
2022-10-07 11:24:30 +0200

    Debugging

--- src/controller.ts
+++ src/controller.ts
@@ -9,7 +9,7 @@ const jhelper = require("../utils/jwt-helpers.ts");
 export const refreshToken = (req: Request, res: Response) => {
   try {
     const refreshT = req.params.refresh_token;
-    console.log(refreshT)
+    console.log(`Refresh Token is ${refreshT}`)
     if (refreshT === null) return res.status(400).json({ error: 'Null Refresh Token.' });
     jwt.verify(refreshT, process.env.REFRESH_TOKEN_SECRET as string, (error: any, user: any) => {
       if (error) return res.status(403).json({ error: error.message });
